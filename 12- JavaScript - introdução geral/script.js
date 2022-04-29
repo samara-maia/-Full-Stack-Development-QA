4263d9ee48cf81a248785c9ae55d37485d96d226
Samara <maiareismaia@gmail.com>
2022-04-29 18:25:29 -0300

    aula 4 - Formas de mostrar (Output)

--- 12- JavaScript - introdução geral/script.js
+++ 12- JavaScript - introdução geral/script.js
@@ -1,1 +1,15 @@
-alert("opa, estou funcionando!");
+//base de todo JavaScript
+// principais coisas para mostrar para o usuário final
+
+
+//a primeira é manipulando o ID do HTML
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+
+//raramente usado 
+document.write("Algum Texto");
+
+// outra forma é usando a janela do navegador
+window.alert("")
+
+//console 
+console.log("A mensagem que inserir aqui, aparecerá no console.")
