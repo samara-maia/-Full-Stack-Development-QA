140111e6ef3d329b1efb89da73cf02786fc2c547
Samara <maiareismaia@gmail.com>
2022-04-29 18:49:12 -0300

    aula 5 - variáveis

---
+++ 12- JavaScript - introdução geral/scrit1.js
@@ -0,0 +1,24 @@
+//criação de variaveis.
+
+//exemplo 1
+var nome = "samara" ;
+var idade = 90;
+
+alert(nome);
+alert(idade);
+
+//exemplo 2 
+var x = 10;
+var y = 15;
+
+var total = x + y ;
+
+alert(total);
+
+//exemplo 3
+var nome = "samara"; //string
+var sobrenome = "Lacerda";
+
+var nomecompleto = nome+" "+sobrenome; //concatenar
+
+alert(nomecompleto);
