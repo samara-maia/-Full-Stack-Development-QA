ebc0c5082264d7f5c06fcbd86fa24eaf2255b59a
Samara <maiareismaia@gmail.com>
2022-04-29 19:30:47 -0300

    aula 8- variaveis

---
+++ 12- JavaScript - introdução geral/script.3.js
@@ -0,0 +1,8 @@
+var nome = "samara";// hoisting 
+//console.log( window.nome);
+
+let nome = "samara";//disponivel apenas na mesma area
+
+const nome = "samara";// contante não podendo modificar o valor
+
+const nome = {nome: 'samara', sobrenome:'lacerda'};//
