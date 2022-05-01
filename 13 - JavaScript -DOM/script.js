08a666aba736ae809427c6cb3635c0de29821e78
Samara <maiareismaia@gmail.com>
2022-05-01 19:13:05 -0300

    aula 3 selecionando elementos 2

--- 13 - JavaScript -DOM/script.js
+++ 13 - JavaScript -DOM/script.js
@@ -2,3 +2,7 @@
 
 document.getElementById('exemplo').innerHTML = "samara";
 // <div id="exemplo">bonieky</div>
+document.getElementsByClassName('lista')[1].innerHTML = 'Item alterado';
+//HTMLcollection(2)[div.lista, div.lista]
+document.getElementsByTagName('...')
+document.getElementsByName('...')
