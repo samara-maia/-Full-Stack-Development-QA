5a28bfb83751176450b93ae8102cc521468650ab
Samara <maiareismaia@gmail.com>
2022-05-02 11:33:26 -0300

    aula 7 - tipos de dados

---
+++ 13 - JavaScript -DOM/script4.js
@@ -0,0 +1,7 @@
+var nome = 'Bonieky';
+var idade = 90;
+var salvo = true; // boolean
+var cidade; // undefined ou null
+
+// no console digita.
+//typeof nome; "o tipo de dado, variavel especifica" ex: string
