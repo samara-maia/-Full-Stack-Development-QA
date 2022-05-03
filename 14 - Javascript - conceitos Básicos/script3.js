b2d96ea2dacfc1b0459550016917254200f5982b
Samara <maiareismaia@gmail.com>
2022-05-03 20:39:48 -0300

    aula 5 - objeto

---
+++ 14 - Javascript - conceitos Básicos/script3.js
@@ -0,0 +1,17 @@
+// array =>  listagem de numerada aparti de 0
+// objeto => listagem nomeada
+
+let carros = [
+    'palio',
+    'Uno',
+    'Corolla',
+    'Ferrari'
+]; //array
+
+let carro = {
+    nome:'fiat',
+    modelo:'Uno',
+    peso:'800kg'
+}; // objeto
+
+document.getElementById("titulo").style.display = "block";
