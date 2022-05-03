3dcb36c16dced2e17bf2edb3f106e12380a11b76
Samara <maiareismaia@gmail.com>
2022-05-03 10:00:49 -0300

    aula 12 - Dimensões

---
+++ 13 - JavaScript -DOM/script7.js
@@ -0,0 +1,11 @@
+//dimençoes de largura
+document.querySelector('.texto').offsetHeight
+
+document.querySelector('.texto').offsetWidth
+//domençoes de altura
+document.querySelector('.texto').clientHeight
+document.querySelector('.texto').clientWidth
+
+//tamanho real do texto do documento
+document.querySelector('.texto').scrollHeight
+document.querySelector('.texto').scrollWidth
