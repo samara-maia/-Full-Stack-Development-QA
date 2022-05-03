65d6cd20d77e1354dc6560bf053c29ade8a8bdbe
Samara <maiareismaia@gmail.com>
2022-05-03 10:16:52 -0300

    aula 13 - Distâncias e Scroll Suave

--- 13 - JavaScript -DOM/script7.js
+++ 13 - JavaScript -DOM/script7.js
@@ -9,3 +9,14 @@ document.querySelector('.texto').clientWidth
 //tamanho real do texto do documento
 document.querySelector('.texto').scrollHeight
 document.querySelector('.texto').scrollWidth
+
+//scroll vertial 
+document.querySelector('.texto').scrollTop
+//scroll horizoltal
+document.querySelector('.texto').scrollLeft
+//tela inteira
+window.scrollY //vertical
+window.screenX//horizoltal
+
+//altera elemento
+document.querySelector('.texto').scrollTo(0, 0)// x e y
