e2fb657c1bac79e606e84420f41190d47cb6f2ba
Samara <maiareismaia@gmail.com>
2022-05-05 10:11:05 -0300

    aula 2 - métodos de string ( 2- 3)

---
+++ 15 - Javascript - Manipulações, Arrow, Ajax, etc./script1.js
@@ -0,0 +1,8 @@
+let nome = 'samara maia';
+
+let resultado = nome.slice(10, 15);//seleciona a parte da string que deseja e numeros negativos
+
+//substring apenas padrão
+//substr filtrar algum tipo de string
+
+console.log(resultado);
