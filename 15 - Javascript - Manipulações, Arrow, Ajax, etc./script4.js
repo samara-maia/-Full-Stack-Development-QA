fb93d184ab118f48af613802bd30eaa41f3a1e98
Samara <maiareismaia@gmail.com>
2022-05-05 11:43:49 -0300

    aula 5 - mpetodo de array 1- 4

---
+++ 15 - Javascript - Manipulações, Arrow, Ajax, etc./script4.js
@@ -0,0 +1,11 @@
+let lista = [ 'ovo','farinha','corante','massa'];
+
+//lista.popo remove o ultimo item do array
+//.shift() remove o primeito
+
+//.push adicionar um novo item
+
+let res = lista.join('')
+// indexOf('') procura um item especifico no array
+//let res = lista;  
+console.log(res);
