283059367b47abb6b9ca107c3e10d026f5d168c4
Samara <maiareismaia@gmail.com>
2022-05-06 19:41:59 -0300

    aula 10  - datas 2-3

---
+++ 15 - Javascript - Manipulações, Arrow, Ajax, etc./data.js
@@ -0,0 +1,11 @@
+let = new Date()
+
+let novoValor = d.getMonth() // mês
+let novoValor = d.getfullYear() // ano
+let novoValor = d.getDay() // dia da semana
+let novoValor = d.getDate() //dia atual
+let novoValor = d.gethours() // hora
+let novoValor = d.getMinutes() // minutos
+let novoValor = d.getSeconds() //segudos
+
+console.log(novoValor)
