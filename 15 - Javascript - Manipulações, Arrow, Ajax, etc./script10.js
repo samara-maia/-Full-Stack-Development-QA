51f1e2908ccff753234cc2420043bd92ab071fb6
Samara <maiareismaia@gmail.com>
2022-05-06 21:02:27 -0300

    aula 12 - matematica

---
+++ 15 - Javascript - Manipulações, Arrow, Ajax, etc./script10.js
@@ -0,0 +1,10 @@
+//aula 12 Matemática
+//let novoValor = Math.PI
+
+//let novoValor = Math.round(3.67) // arredondar
+
+//let novoValor = Math.floor(3.53) // arredonda p/ baixo
+// pra cima Math.cail
+
+let novoValor = Math.floor(Math.random() * 100)
+console.log(novoValor)
