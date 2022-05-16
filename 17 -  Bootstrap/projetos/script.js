0212c0c5c2dc014f4a3e74d11c9738e2e242648b
Samara <maiareismaia@gmail.com>
2022-05-16 14:06:08 -0300

    aula 30 - porjeto filmes em cartaz

---
+++ 17 -  Bootstrap/projetos/script.js
@@ -0,0 +1,24 @@
+$(function () {
+  $.ajax({
+    url: 'https://api.b7web.com.br/cinema/',
+    type: 'GET',
+    dataType: 'json',
+    beforSend: function () {
+      $('.filmes').html('<div class="col-md-12">carregando...s</div>')
+    },
+    success: function (json) {
+      var html = ''
+
+      for (var i in json) {
+        html +=
+          '<div class="col-md-4"><div class="filme"><img src=" ' +
+          json[i].avatar +
+          '"/>' +
+          json[i].titulo +
+          '</div></div>'
+      }
+
+      $('.filmes').html(html)
+    }
+  })
+})
