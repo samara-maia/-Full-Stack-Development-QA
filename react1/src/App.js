21b4ed85965361a50a11debc923d272b5a521491
Samara <maiareismaia@gmail.com>
2022-05-18 21:02:50 -0300

    aula 2 Classes e Funções

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
+/*class App extends React.Component {
+  render() {
+    return <h1>Testando 1,2,3</h1>
+  }
+}*/
+/*
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <h1>Testando 3,4,5</h1>
+}*/
 
-export default App;
+/*
+let App = () => {
+  return <h1>Testando 7,8,9</h1>
+}*/
+
+let App = () => <h1>Testando 10, 11, 12</h1>
+export default App
