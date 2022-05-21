550a3b63fce8a658308e3d05da5720bf1bfeda00
Samara <maiareismaia@gmail.com>
2022-05-21 16:36:07 -0300

    aula 7  Estilizando c/ StyledComponents (2-3)

--- react1/src/App2.js
+++ react1/src/App2.js
@@ -2,20 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Site = styled.div`
- width: 400px;
- height: 400px;
- background-color: #00ff00;
+  width: 400px;
+  height: 400px;
+  background-color: #00ff00;
 `
 
 const Botao = styled.button`
   font-size: 19px;
   padding: 10px 15px;
+  background-color: ${props => (props.ativo === true ? '#0000FF' : '#CCC')};
+  color: ${props => (props.ativo === true ? '#FFF' : '#000')};
 `
 
 function App() {
   return (
     <Site>
-      <Botao>Clique aqui</Botao>
+      <Botao ativo={true}>Clique aqui</Botao>
+      <Botao ativo={false}>Clique aqui</Botao>
     </Site>
 
     /*<Site>
