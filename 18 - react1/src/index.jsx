7f31839718bd0c9df73c893022fe43bd97cec64f
Samara <maiareismaia@gmail.com>
2022-05-24 20:26:54 -0300

    aula 18 Marcando como feito

--- 18 - react1/src/index.jsx
+++ 18 - react1/src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // 👇️ For client createRoot or hydrateRoot
 import * as ReactDOMClient from 'react-dom/client';
 //import ReactDOM from 'react-dom'
-import App from './App8'
+import App from './App9'
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'))
 root.render(
