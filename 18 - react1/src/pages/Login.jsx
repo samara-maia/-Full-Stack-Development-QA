3459d1c4dbe98261714fd1e3a7551631ffe429ee
Samara <maiareismaia@gmail.com>
2022-06-07 21:45:21 -0300

    estado global: aula 5 Redux em várias telas

--- 18 - react1/src/pages/Login.jsx
+++ 18 - react1/src/pages/Login.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
 
-function Login() {
+import React from 'react'
+import {connect} from 'react-redux'
+
+
+function Login(props) {
+  const handleFelipe = () => {
+    props.setName('Felipe')
+  }
   return (
     <div>
       <h4>Página LOGIN</h4>
+      O nome é: {props.name}
+
+      <br/><br/>
+      <button onClick={handleFelipe}>Trocar nome para Felipe</button>
     </div>
   )
 }
 
-export default Login
+const mapStateToProps = (state) => ({
+  name: state.usuario.name
+})
+
+const mapDispatchToProps = dispatch =>({
+  setName: (newName) => dispatch ({
+    type: 'SET_NAME',
+    payload: {name: newName}
+  })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps) (Login)
