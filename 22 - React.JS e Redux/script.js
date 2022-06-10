ba6c6d921590ee9ebaec06f8130aa44b889b6b2a
Samara <maiareismaia@gmail.com>
2022-06-10 20:48:16 -0300

    aula 2 - Olá Mundo em React

---
+++ 22 - React.JS e Redux/script.js
@@ -0,0 +1,5 @@
+//let app = document.getElementById('app') uma opção.
+//ReactDOM.render('Ola Mundo', app)
+ReactDOM.render('Ola Mundo', document.getElementById('app'))
+
+ReactDOM.render('Olá Segndo', document.getElementById('app2'))
