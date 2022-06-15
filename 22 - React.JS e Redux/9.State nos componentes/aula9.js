66fb8a63512631b51a250ebfdb4b64e489da1c83
Samara <maiareismaia@gmail.com>
2022-06-15 15:37:29 -0300

    aula 9 state nos componentes

---
+++ 22 - React.JS e Redux/9.State nos componentes/aula9.js
@@ -0,0 +1,28 @@
+class Relogio extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hora: '00:00:00'
+    }
+
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Horário</h1>
+        <h2>{this.state.hora}</h2>
+      </div>
+    )
+  }
+}
+
+let elemento = (
+  <div>
+    <Relogio />
+  </div>
+)
+
+ReactDOM.render(elemento, document.getElementById('app'))
