a1e37685cd0298d8b441d9d8e611741d9fee56b4
Samara <maiareismaia@gmail.com>
2022-06-16 12:00:09 -0300

    add subpasta com a ordem da plataforma

---
+++ 13 - JavaScript -DOM/13 - Distâncias e Scroll Suave/script.js
@@ -0,0 +1,10 @@
+//scroll vertial
+document.querySelector('.texto').scrollTop
+//scroll horizoltal
+document.querySelector('.texto').scrollLeft
+//tela inteira
+window.scrollY //vertical
+window.screenX //horizoltal
+
+//altera elemento
+document.querySelector('.texto').scrollTo(0, 0) // x e y
