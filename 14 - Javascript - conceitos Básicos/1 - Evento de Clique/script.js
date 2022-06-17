4034d2aa1cd25d6d0597c621d99737dc08755ecf
Samara <maiareismaia@gmail.com>
2022-06-17 15:00:04 -0300

    evento clique

---
+++ 14 - Javascript - conceitos Básicos/1 - Evento de Clique/script.js
@@ -0,0 +1,7 @@
+//o evendo colocado no onclick codg. javascript irá funcionar
+document.getElementByTd('titulo').innerHTML = 'Texto 2'
+
+// Alterar o propio botão
+this.innerHTML = 'Clicou'
+//quando o a mouse estiver sobre o elemento
+onmouseover = ''
