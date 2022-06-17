0b7bb50b4875d7150d7a9fc5197e40cd77774a65
Samara <maiareismaia@gmail.com>
2022-06-17 16:05:33 -0300

    array

---
+++ 14 - Javascript - conceitos Básicos/4 - Array/script.js
@@ -0,0 +1,18 @@
+// array =>  listagem de numerada aparti de 0
+// em uma única varial adcionar varios valores diferentes.
+
+
+//let carros = ['Palio', 'Uno', 'Corolla', 'Ferrari']
+
+
+//console.log(carros[0])
+
+let carros = [
+  'Palio',
+  'Uno',
+  'Corolla',
+  'Ferrari'
+  function(){
+    console.log('Testando 1, 2, 3...')
+  }
+]
