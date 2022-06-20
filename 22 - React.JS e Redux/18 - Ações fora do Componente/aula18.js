fec6fb753a33ffbb96bb3c1105f73219d1257920
Samara <maiareismaia@gmail.com>
2022-06-20 20:01:39 -0300

    aula 18 - açoes fora do componente

---
+++ 22 - React.JS e Redux/18 - Ações fora do Componente/aula18.js
@@ -0,0 +1,42 @@
+class Botao extends React.Component {
+  render() {
+    return (
+      <button
+        onClick={() => {
+          if (this.props.status === 1) {
+            this.props.funcaoTrocar(0)
+          } else {
+            this.props.funcaoTrocar(1)
+          }
+        }}
+      >
+        {this.props.status === 1 ? 'DESABILITAR' : 'HABILITAR'}
+      </button>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: 1
+    }
+    this.trocarStatus = this.trocarStatus.bind(this)
+  }
+  trocarStatus(novoStatus) {
+    this.setState({ status: novoStatus })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.status == 1 ? 'HABILITADO' : 'DESABITADO'}</p>
+
+        <Botao status={this.state.status} funcaoTrocar={this.trocarStatus} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
