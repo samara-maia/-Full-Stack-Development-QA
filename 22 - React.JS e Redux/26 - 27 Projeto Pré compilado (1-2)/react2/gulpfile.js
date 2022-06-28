cb7ae2632583adc8b1f3449f5837fb24e93cb310
Samara <maiareismaia@gmail.com>
2022-06-27 22:47:38 -0300

    aula 26 intalações e configurações das dependencia

---
+++ 22 - React.JS e Redux/26 - 27 Projeto Pré compilado (1-2)/react2/gulpfile.js
@@ -0,0 +1,34 @@
+const { src, parallel, dest } = require('gulp')
+const named = require('vinyl-named')
+const webpack = require('webpack-stream')
+
+function html() {
+  return src('src/*.html').pipe(dest('public/'))
+}
+
+function js() {
+  return src('src/js/index.js')
+    .pipe(named())
+    .pipe(
+      webpack({
+        mode: 'production',
+        output: {
+          filename: '[name].js'
+        },
+        module: {
+          rules: [
+            {
+              test: /\.(js|jsx)$/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+              }
+            }
+          ]
+        }
+      })
+    )
+    .pipe(dest('public/assets/js/'))
+}
